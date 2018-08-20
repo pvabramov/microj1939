cd541a2077da59401bb57838e0e2b83b62d700ee
abramov-pv <abramov-pv@dep124.avt>
2018-08-20 11:47:55 +0500

    Добавление файла шаблона для BSP

---
+++ samples/j1939_bsp_sample.c
@@ -0,0 +1,40 @@
+
+#include <string.h>
+
+#include "j1939_bsp.h"
+
+
+int j1939_bsp_lock(void) {
+    return 0;
+}
+
+
+void j1939_bsp_unlock(int level) {
+    
+}
+
+
+void j1939_bsp_CAN_bus_off(void) {
+    
+}
+
+
+uint32_t j1939_bsp_get_time() {
+    return 0;
+}
+
+
+void j1939_bsp_mdelay(uint32_t ms) {
+    
+}
+
+
+int j1939_bsp_CAN_recv(j1939_primitive *const primitive) {
+    return 0;
+}
+
+
+int j1939_bsp_CAN_send(const j1939_primitive *const primitive) {
+    return 0;
+}
+
