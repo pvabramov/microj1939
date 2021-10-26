e1f406e5f53f65677b716db258fdb3f93bf0f1ba
abramov-pv <abramov-pv@dep124.avt>
2021-10-26 12:11:19 +0500

    Добавлены некоторые PGN из документа SAE J1939-21

--- J1939/j1939_std_pgn.h
+++ J1939/j1939_std_pgn.h
@@ -14,6 +14,9 @@
 #define J1939_STD_PGN_RQST              59904U
 #define J1939_STD_PGN_RQST_DLC          3
 
+#define J1939_STD_PGN_RQST2             51456U
+#define J1939_STD_PGN_RQST2_DLC         8
+
 #define J1939_STD_PGN_TPDT              60160U
 #define J1939_STD_PGN_TPDT_DLC          8
 
@@ -24,6 +27,15 @@
 #define J1939_STD_PGN_ACLM_DLC          8
 
 #define J1939_STD_PGN_PROPA             61184U
+// no DLC
+
+#define J1939_STD_PGN_PROPA2            126720U
+// no DLC
+
+#define J1939_STD_PGN_PROPB(x)          (65280U + (x))
+// no DLC
+
+#define J1939_STD_PGN_TRANSFER          51712U
 // no DLC
 
 #endif /* J1939_STD_PGN_H_ */
